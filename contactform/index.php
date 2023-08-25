795549e7c2c81ede5fe8b23c4eadd4cf83e94fd3
hariharan-16 <134910281+hariharan-16@users.noreply.github.com>
2023-08-25 16:39:29 -0400

    changes on index page

---
+++ contactform/index.php
@@ -0,0 +1,12 @@
+<body>
+  <div style="display: flex; justify-content: center; align-item: center; height: 100vh">
+    <form id="contact" action="send_mail.php" method="post" style="display: flex; justify-content: center; align-item: center; flex-direction: column">
+      <h3>Contact Form</h3>
+      <input placeholder="Your Name" name="name" type="text" tabindex="1"><br>
+      <input placeholder="Email Address" name="email" type="email" tabindex="2"><br>
+      <textarea placeholder="Enter your Thoughts..!!" name="comments" tabindex="3"></textarea><br>
+      <input placeholder="Phone Number" name="tel" type="number" tabindex="4"><br>
+      <button type="submit" name="submit">Submit Now</button>
+    </form>
+  </div>
+</body>
